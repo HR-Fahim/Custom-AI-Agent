f7de3b5dc630a3d5d7b71dd727a577acd11915a2
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-09-10 05:21:10 +0600

    first commit

---
+++ project/init.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Loaded .env
+
+# Model initialization
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-3.5-turbo")
+
+# Test Code to check API interrogation
+# from langchain_core.messages import HumanMessage
+
+# response = model.invoke([HumanMessage(content="How are you?")])
+# print(response.content)
